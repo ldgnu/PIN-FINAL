14752b2947d64dd5b04701e87a2b0598866e9aec
LDGNU <javikore@gmail.com>
2023-11-26 11:46:48 -0300

    Ordenando todo

---
+++ 01-Bastion/ec2_user_data.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+echo "Update Packages"
+sudo apt-get update -y && sudo apt upgrade -y
+
+echo "Installing AWS CLI"
+sudo apt install unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+echo "Installing kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+kubectl version --client
+
+echo "Installing ekctl"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+export PATH=$PATH:/usr/local/bin
+echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+eksctl version
+
+# Para que docker ? 
+# echo "Installing docker"
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo chmod +x get-docker.sh
+# sudo bash get-docker.sh
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# docker --version
+
+echo "Installing Helm"
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+sudo chmod 700 get_helm.sh
+sudo bash get_helm.sh
+helm version
+
+echo "Installing terraform "
+sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+
+terraform -version
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# PIN-FINAL
-Trabajo PIN Final Diplomatura DevOps - Mundose
+# Proyecto Integrador Final – Javier Solis - MundoSE
+Índice:
+
+Configuración del Bastion Host en AWS
+1.1. Crear una instancia de EC2 para el Bastion Host
+1.3. Ejecutar script de instalación (aws-cli, kubectl, helm, terraform)
+1.2. Conectar al Bastion Host
+
+Configuración del Cluster en Amazon EKS
+2.1. Crear un rol de IAM para el EKS
+2.2. Crear un clúster de EKS
+2.3. Configurar kubectl para el nuevo clúster
+2.4. Desplegar Nginx en el clúster
+2.5. Desplegar Prometheus en el clúster
+2.6. Desplegar Grafana en el clúster
+2.7. Configurar las reglas de seguridad del clúster
+
+1. Configuración del Bastion Host en AWS:
+1.1. Crear una instancia de EC2 para el Bastion Host:
+
+Levantamos una instancia EC2 para utilizar de Bastion.
+Caracteristicas Region: us-east-2 
+Sistema Operativo : Ubuntu Server 20.04 
+Family (Tipo): t2.micro
+
